03077e75b1690c4cca062432cf39a651bc840bff
Tushar Borole <tushariscoolster@gmail.com>
2015-06-17 22:58:47 +0530

    form chages

--- angular-resetform.js
+++ angular-resetform.js
@@ -3,15 +3,37 @@ angular.module('angular-resetform').directive('resetForm', function ($parse, $co
         require: ['^form', 'ngModel'],
         link: function (scope, elm, attr, formCtrl) {
 
-            formCtrl[0].$resetFormData = function () {
 
-                $parse(attr.ngModel).assign(scope, formCtrl[1].initialValue);
+            formCtrl[0].$resetFormData = function () {
+                if (angular.isDefined(attr.resetFormValue)) { // to set value on form rest
+                    $parse(attr.ngModel).assign(scope, formCtrl[1].initialValue);
+                } else {
+                    $parse(attr.ngModel).assign(scope, attr.resetFormValue);
+
+                }
+
+
+
             };
             scope.$watch(attr.ngModel, function (value) {
-                console.log(formCtrl[0])
-                if (formCtrl[1].$pristine) {
+
+
+
+                if (formCtrl[1].$pristine) { //if form is touched or not
+
+
                     formCtrl[1].initialValue = value;
+
+
+                    if (angular.isDefined(attr.resetFormClear)) { // clear value on reset
+                        formCtrl[1].initialValue = '';
+                    }
+
+
+
                 }
+
+
             });
 
 
